da2bd94febf1d67b9755ab80e0635167760663ff
Jud <jud.howland@gmail.com>
2020-06-27 22:07:02 -0500

    updated all jquery to dynamically add html and updated readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ $("#city-search-button").on("click", function () {
 });
 
 function addCityToList(cityName) {
-    var newRow = $("<button>").addClass("new-button").text(cityName);
+    var newRow = $("<button>").addClass("new-button m-2 p-1").text(cityName);
     $("#previous-cities").append(newRow);
 };
 
@@ -17,17 +17,22 @@ $("#previous-cities").on("click", "button", function() {
 })
 
 function getTodayWeather(cityName) {
-    fetch(' https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&APPID=d4b74d5339d458e26b64aecdf7a1e8c0')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&APPID=d4b74d5339d458e26b64aecdf7a1e8c0')
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
+                    
                     var iconApi = ("http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png");
 
-                    $("#city-name-display").text(cityName + " (" + new Date().toLocaleDateString() + ")");
+                    var cityTitle = $("<h3>").text(cityName + " (" + new Date().toLocaleDateString() + ")");
+                    var todayIcon = $("<img>").attr("src", iconApi);
+    
+                    $("#city-name-display").html(cityTitle).append(todayIcon);
 
-                    $("#today-icon").attr("src", iconApi);
+                    // $("#today-icon").attr("src", iconApi);
 
                     displayTodayWeather(data);
+                    getUvIndex(data);
                 });
             } else {
                 alert("Error: " + response.StatusText);
@@ -35,11 +40,38 @@ function getTodayWeather(cityName) {
         });
 };
 
+function getUvIndex (data) {
+    var cityLat = data.coord.lat;
+    var cityLon = data.coord.lon;
+                   
+    var uvApi = fetch("http://api.openweathermap.org/data/2.5/uvi?appid=d4b74d5339d458e26b64aecdf7a1e8c0&lat=" + cityLat +"&lon=" + cityLon)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    var cityUv = $("<p>").text("UV Index: ");
+                    var btn = $("<span>").addClass("text-white btn btn-sm").text(data.value);
+                    if (data.value < 2 ) {
+                        btn.addClass("bg-success");
+                    } else if (data.value < 7) {
+                        btn.addClass("bg-warning");
+                    } else {
+                        btn.addClass("bg-danger");
+                    }
+
+                    $("#city-weather-data").append(cityUv.append(btn));
+                });
+            }
+        });
+};
+
 function displayTodayWeather(data) {
-    $("#today-temp").text(data.main.temp + "°F");
-    $("#today-humidity").text(data.main.humidity + "%");
-    $("#today-wind-speed").text(data.wind.speed + " MPH");
+    var temp = $("<p>").text("Temperature: " + data.main.temp + "°F");
+    var humidity = $("<p>").text("Humidity: " + data.main.humidity + "%");
+    var wind = $("<p>").text("Wind Speed: " + data.wind.speed + " MPH");
+    // var uvIndex = $("<p>").
     //  $("#city-uv-index").text(data.)
+
+    $("#city-weather-data").html(temp).append(humidity).append(wind);
 };
 
 function getForecastWeather(cityName) {
@@ -47,12 +79,13 @@ function getForecastWeather(cityName) {
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    console.log(data);
                     displayForecastWeather(data);
                 });
             } else {
                 alert("Error: " + response.StatusText);
             }
+            var forecastTitle = $("<h3>").addClass("h3 col-sm-12 col-md-12 m-1").text("5-Day Forecast:");
+            $("#forecast-title").html(forecastTitle);
         });
 
 };
@@ -60,15 +93,15 @@ function getForecastWeather(cityName) {
 function displayForecastWeather(data) {
     for (var i=0; i < data.list.length; i++) {
         if (data.list[i].dt_txt.indexOf("12:00:00") !== -1) {
-            var forecastCard = $("<div>").addClass("col-12 col-md-2 text-white bg-primary m-1 p-2");
+            var forecastCard = $("<div>").addClass("col-sm-12 col-md-2 text-white bg-primary m-2 p-2");
             var forecastDate = $("<h6>").text(new Date(data.list[i].dt_txt).toLocaleDateString());
             var forecastIcon = $("<img>").attr("src", ("http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png"));
-            var forecastTemp = $("<p>").addClass("text-sm").text("Temp:" + data.list[i].main.temp_max + " °F");
-            var forecastHumidity = $("<p>").addClass("text-sm").text("Humidity:" + data.list[i].main.humidity + "%");
+            var forecastTemp = $("<p>").addClass("font-weight-lighter").text("Temp: " + data.list[i].main.temp_max + " °F");
+            var forecastHumidity = $("<p>").addClass("font-weight-lighter").text("Humidity: " + data.list[i].main.humidity + "%");
 
             // add the sections together
-            forecastCard.append(forecastDate, forecastIcon, forecastTemp, forecastHumidity);
-            $("#forecast-section").append(forecastCard);
+            $("#forecast-title").append(forecastCard.append(forecastDate, forecastIcon, forecastTemp, forecastHumidity));
+            // $("#forecast-title").append(forecastCard);
         }
     }
 };
